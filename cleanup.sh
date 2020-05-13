8b6f5fb5261028f48ce4e5ba9575b56f2035c681
phantom9999 <1057369361@qq.com>
2020-05-14 00:02:11 +0800

    fix

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,4 @@
 # `$*` expands the `args` supplied in an `array` individually 
 # or splits `args` in a string separated by whitespace.
 sh -c "echo $*"
-cd $1
-sh $4
 
--- main.sh
+++ main.sh
@@ -3,6 +3,8 @@
 # `$*` expands the `args` supplied in an `array` individually 
 # or splits `args` in a string separated by whitespace.
 sh -c "echo $*"
-cd $1
-sh $2
+echo ${GITHUB_WORKSPACE}
+echo ${INPUT_SHELL_MAIN}
+echo ${INPUT_SHELL_PRE}
+echo ${INPUT_SHELL_POST}
 

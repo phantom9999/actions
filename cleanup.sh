e6e7bedcc0d275c22766a74a766947ca10edf5a3
phantom9999 <1057369361@qq.com>
2020-05-13 23:46:59 +0800

    fix

--- cleanup.sh
+++ cleanup.sh
@@ -3,5 +3,6 @@
 # `$*` expands the `args` supplied in an `array` individually 
 # or splits `args` in a string separated by whitespace.
 sh -c "echo $*"
-sh $3
+cd $1
+sh $4
 
--- main.sh
+++ main.sh
@@ -3,5 +3,6 @@
 # `$*` expands the `args` supplied in an `array` individually 
 # or splits `args` in a string separated by whitespace.
 sh -c "echo $*"
-sh $1
+cd $1
+sh $2
 
